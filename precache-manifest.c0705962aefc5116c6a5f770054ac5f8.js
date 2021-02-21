b92c0522ec2a1f7e7ce09b89c3abd94b7e3ce8e3
mortartroop <Nicolas.Pismiris@gmail.com>
2021-02-21 13:28:19 +0800

    Updates

---
+++ precache-manifest.c0705962aefc5116c6a5f770054ac5f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc3afbe8b5cd48640077fc178210090",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "d153455e9205abc384d4",
+    "url": "/react-invoice-generator/static/css/2.61833881.chunk.css"
+  },
+  {
+    "revision": "0fafecbfc2fa7691f3f0",
+    "url": "/react-invoice-generator/static/css/main.c39616d1.chunk.css"
+  },
+  {
+    "revision": "d153455e9205abc384d4",
+    "url": "/react-invoice-generator/static/js/2.ae05f89c.chunk.js"
+  },
+  {
+    "revision": "6d434ff574ba6e0632e145d5ccc24beb",
+    "url": "/react-invoice-generator/static/js/2.ae05f89c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fafecbfc2fa7691f3f0",
+    "url": "/react-invoice-generator/static/js/main.8ee4260d.chunk.js"
+  },
+  {
+    "revision": "aabb303fe90f96d72bbb",
+    "url": "/react-invoice-generator/static/js/runtime-main.33c79cdc.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/react-invoice-generator/static/media/download.5af81432.svg"
+  }
+]);
